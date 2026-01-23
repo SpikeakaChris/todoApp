e6867c7413e0673e1c68d643af4d78920288ac3f
SpikeakaChris <chmspike@googlemail.com>
2026-01-23 15:03:10 +0100

    Navbar

--- todoApp/src/app/app.ts
+++ todoApp/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { Header } from './header/header';
+import { Header,} from './header/header';
+import { Navbar } from './navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [Header,],
+  imports: [Header, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
