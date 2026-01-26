1905a196a2fffa4cbf0f457d35a6ebd8c70bd9f4
SpikeakaChris <chmspike@googlemail.com>
2026-01-26 17:53:05 +0100

    buttons

--- todoApp/src/app/shared/components/buttons/buttons.ts
+++ todoApp/src/app/shared/components/buttons/buttons.ts
@@ -7,6 +7,5 @@ import { Component, input } from '@angular/core';
   styleUrl: './buttons.css',
 })
 export class Buttons {
-    label = input<string>();
-    variant = input<'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark' >('primary');
+    btntext = input<string>();    
 }
