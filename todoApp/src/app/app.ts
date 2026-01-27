6ade718bb0b1b9d38c4114b48e81d98f7cdcb41e
SpikeakaChris <chmspike@googlemail.com>
2026-01-27 15:03:09 +0100

    information angelegt

--- todoApp/src/app/app.ts
+++ todoApp/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { Header,} from './header/header';
 import { Navbar } from './navbar/navbar';
 import { Mainnav } from './mainnav/mainnav';
+import { Information } from "./information/information";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Navbar, Mainnav],
+  imports: [Header, Navbar, Mainnav, Information],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ todoApp/src/app/information/information.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Buttons } from '../shared/components/buttons/buttons';
+
+@Component({
+  selector: 'app-information',
+  imports: [Buttons],
+  templateUrl: './information.html',
+  styleUrl: './information.css',
+})
+export class Information {
+
+}
