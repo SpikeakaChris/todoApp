fadcca6cb1bd8470e83570056aa155c6024d0209
SpikeakaChris <chmspike@googlemail.com>
2026-01-28 15:01:34 +0100

    eingabefelder

--- todoApp/src/app/information/information.ts
+++ todoApp/src/app/information/information.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Buttons } from '../shared/components/buttons/buttons';
 
 @Component({
   selector: 'app-information',
-  imports: [Buttons],
+  imports: [CommonModule, Buttons],
   templateUrl: './information.html',
   styleUrl: './information.css',
 })
 export class Information {
 
+
+
+    badges: { name: string; img: string }[] = [];
+
+    onSelect(event: Event) {
+        const select = event.target as HTMLSelectElement;
+        const option = select.options[select.selectedIndex];
+
+        const name = option.value;
+        const img = option.dataset['img'];
+
+        if (!img) return;
+
+        this.badges.push({ name, img });
+    }
 }
